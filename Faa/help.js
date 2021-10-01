cbc3cf585de9eaf09901f087269e45856c54cfbc
xiecaa <91745793+xiecaa@users.noreply.github.com>
2021-10-01 23:25:12 +0700

    Add files via upload

---
+++ Faa/help.js
@@ -0,0 +1,99 @@
+const rules = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { return`
+╭───「 *PERATURAN* 」
+┴
+❏ JANGAN VC/VON BOT!!
+❏ JANGAN SPAM!!
+❏ KASIH WAKTU 5/10 Dtk
+❏ KALO GA DI JAWAB SAMA BOT
+❏ PENDING/EROR/OFF
+┬
+╰───「 *PINKY BOT* 」
+`
+}
+
+exports.rules = rules
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (6060));
+  var minutes = Math.floor(seconds % (6060) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik`
+}
+
+// bahasa list
+const bahasa = (prefix) => {
+return `
+List Bahasa Untuk Command ${prefix}tts
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
+
+// Limit
+const limitend = (pushname) => {
+        return`*Maaf ${pushname} Limit Anda Habis*\n*limit di riset setiap 12:00 wib tengah malam*`
+}
+
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: *${limitCounts}*
+`
+}
+
+exports.limitend = limitend
+exports.limitcount = limitcount
---
+++ Faa/pinkyca.js
@@ -0,0 +1,434 @@
+// Base FxSx FdlX
+// Remake Pinky
+// Scpirt Bot Ini Jangan Di Jual!!
+exports.aboutMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *ABOUT PINKY* 」
+┴𖧹
+${fx} ${prefix}report
+${fx} ${prefix}info
+${fx} ${prefix}donasi
+${fx} ${prefix}owner
+${fx} ${prefix}speed
+${fx} ${prefix}limit
+${fx} ${prefix}totaluser
+${fx} ${prefix}chatlist
+${fx} ${prefix}blocklist
+${fx} ${prefix}banlist
+${fx} ${prefix}listcmd
+${fx} ${prefix}bahasa
+${fx} ${prefix}rules
+┬𖧹
+╰────────𖧹
+`
+}
+exports.grupMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *MENU GRUP* 」
+┴𖧹
+${fx} ${prefix}modeanime on/off
+${fx} ${prefix}antilink on/off
+${fx} ${prefix}antibadword on/off
+${fx} ${prefix}welcome on/off
+${fx} ${prefix}grup buka/tutup
+${fx} ${prefix}nsfw on/off
+${fx} ${prefix}ganteng
+${fx} ${prefix}cantik
+${fx} ${prefix}beban
+${fx} ${prefix}opentime
+${fx} ${prefix}closetime
+${fx} ${prefix}ownergrup
+${fx} ${prefix}setpp
+${fx} ${prefix}infogrup
+${fx} ${prefix}add
+${fx} ${prefix}kick
+${fx} ${prefix}kicktime
+${fx} ${prefix}promote
+${fx} ${prefix}demote
+${fx} ${prefix}setname
+${fx} ${prefix}setdesc
+${fx} ${prefix}linkgrup
+${fx} ${prefix}tagme
+${fx} ${prefix}hidetag
+${fx} ${prefix}tagall
+${fx} ${prefix}mentionall
+${fx} ${prefix}jadian
+${fx} ${prefix}fitnah
+${fx} ${prefix}listbadword
+${fx} ${prefix}listadmin
+${fx} ${prefix}liston
+┬𖧹
+╰────────𖧹
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *MENU FUN* 」
+┴𖧹
+${fx} ${prefix}afk
+${fx} ${prefix}testime
+${fx} ${prefix}hilih
+${fx} ${prefix}quotes
+${fx} ${prefix}quotesmotivasi
+${fx} ${prefix}quotesnasehat
+${fx} ${prefix}quotesislami
+${fx} ${prefix}darkjokes
+${fx} ${prefix}bucin
+${fx} ${prefix}pantun
+${fx} ${prefix}img
+${fx} ${prefix}wallpaper
+${fx} ${prefix}katacinta
+${fx} ${prefix}spamsms
+${fx} ${prefix}hekerbucin
+${fx} ${prefix}katailham
+${fx} ${prefix}gantengcek
+${fx} ${prefix}cantikcek
+${fx} ${prefix}artinama
+${fx} ${prefix}persengay
+${fx} ${prefix}pbucin
+${fx} ${prefix}bpfont
+${fx} ${prefix}mimpi
+${fx} ${prefix}readmore
+${fx} ${prefix}tebakgambar
+${fx} ${prefix}meme
+${fx} ${prefix}anime
+${fx} ${prefix}apakah
+${fx} ${prefix}kapankah
+${fx} ${prefix}bisakah
+${fx} ${prefix}rate
+${fx} ${prefix}watak
+${fx} ${prefix}hobby
+${fx} ${prefix}truth
+${fx} ${prefix}dare
+${fx} ${prefix}triggered
+${fx} ${prefix}gay
+${fx} ${prefix}sticker
+${fx} ${prefix}swm
+${fx} ${prefix}takes
+┬𖧹
+╰────────𖧹
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *DOWNLOAD MENU* 」
+┴𖧹
+${fx} ${prefix}quranaudio
+${fx} ${prefix}quran
+${fx} ${prefix}quransurah
+${fx} ${prefix}asmaulhusna
+${fx} ${prefix}tomp3
+${fx} ${prefix}igstalk
+${fx} ${prefix}play
+${fx} ${prefix}play2
+${fx} ${prefix}ytmp3
+${fx} ${prefix}smule
+${fx} ${prefix}joox
+${fx} ${prefix}tiktokstalk
+${fx} ${prefix}tiktok
+${fx} ${prefix}tiktokaudio
+${fx} ${prefix}toimg
+${fx} ${prefix}asupan
+${fx} ${prefix}ocr
+┬𖧹
+╰────────𖧹
+`
+}
+exports.mediaMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *MEDIA MENU* 」
+┴𖧹
+${fx} ${prefix}kbbi
+${fx} ${prefix}wait
+${fx} ${prefix}jadwaltvnow
+${fx} ${prefix}brainly
+${fx} ${prefix}resepmakanan
+${fx} ${prefix}beritahoax
+${fx} ${prefix}map
+${fx} ${prefix}translate
+${fx} ${prefix}infocovid
+${fx} ${prefix}infogempa
+${fx} ${prefix}infonomor
+${fx} ${prefix}apkpure
+${fx} ${prefix}jamdunia
+${fx} ${prefix}trendtwit
+${fx} ${prefix}google
+┬𖧹
+╰────────𖧹
+`
+}
+exports.audioMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *MENU AUDIO* 」
+┴𖧹
+${fx} ${prefix}gemuk
+${fx} ${prefix}tupai
+${fx} ${prefix}bass
+${fx} ${prefix}slow
+${fx} ${prefix}fast
+${fx} ${prefix}tts
+┬𖧹
+╰────────𖧹
+`
+}
+exports.makerMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *MAKER MENU* 」
+┴𖧹
+${fx} ${prefix}darkneon
+${fx} ${prefix}candlemug
+${fx} ${prefix}lovemsg
+${fx} ${prefix}mugflower
+${fx} ${prefix}narutobanner
+${fx} ${prefix}paperonglass
+${fx} ${prefix}romancetext
+${fx} ${prefix}shadowtext
+${fx} ${prefix}coffeecup
+${fx} ${prefix}coffeecup2
+${fx} ${prefix}glowingneon
+${fx} ${prefix}underwater2
+${fx} ${prefix}hpotter
+${fx} ${prefix}woodblock
+${fx} ${prefix}jokerlogo
+${fx} ${prefix}glitchtext
+${fx} ${prefix}crosslogo
+${fx} ${prefix}naruto
+${fx} ${prefix}flowertext
+${fx} ${prefix}silktext
+${fx} ${prefix}flametext
+${fx} ${prefix}glowtext
+${fx} ${prefix}skytext
+${fx} ${prefix}cslogo
+${fx} ${prefix}pixelate
+${fx} ${prefix}pixelate2
+${fx} ${prefix}lithgtext
+${fx} ${prefix}crismes
+${fx} ${prefix}bneon
+${fx} ${prefix}matrix
+${fx} ${prefix}breakwall
+${fx} ${prefix}dropwater
+${fx} ${prefix}leavest
+${fx} ${prefix}darkjokes
+${fx} ${prefix}darklogo
+${fx} ${prefix}logobp
+${fx} ${prefix}goodgirl
+${fx} ${prefix}bucinserti
+${fx} ${prefix}pacarserti
+${fx} ${prefix}fuckgirl
+${fx} ${prefix}fuckboy
+${fx} ${prefix}goodboy
+${fx} ${prefix}badboy
+${fx} ${prefix}badgirl
+${fx} ${prefix}mlwall
+${fx} ${prefix}shadow2
+${fx} ${prefix}cup
+${fx} ${prefix}cup2
+${fx} ${prefix}romance2
+${fx} ${prefix}smoke
+${fx} ${prefix}burnpaper
+${fx} ${prefix}lovemessage
+${fx} ${prefix}undergrass
+${fx} ${prefix}love
+${fx} ${prefix}coffe
+${fx} ${prefix}woodheart
+${fx} ${prefix}flowerheart
+${fx} ${prefix}woodenboard
+${fx} ${prefix}summer3d
+${fx} ${prefix}wolfmetal
+${fx} ${prefix}nature3d
+${fx} ${prefix}underwater
+${fx} ${prefix}golderrose
+${fx} ${prefix}summernature
+${fx} ${prefix}letterleaves
+${fx} ${prefix}glowingneon
+${fx} ${prefix}falleaves
+${fx} ${prefix}flamming
+${fx} ${prefix}potter
+${fx} ${prefix}carvedwood
+${fx} ${prefix}wetglass
+${fx} ${prefix}multicolor3d
+${fx} ${prefix}watercolor
+${fx} ${prefix}luxurygold
+${fx} ${prefix}galaxywallpaper
+${fx} ${prefix}lighttext
+${fx} ${prefix}beautifulflower
+${fx} ${prefix}puppycute
+${fx} ${prefix}royaltext
+${fx} ${prefix}heartshaped
+${fx} ${prefix}birthdaycake
+${fx} ${prefix}galaxystyle
+${fx} ${prefix}hologram3d
+${fx} ${prefix}greenneon
+${fx} ${prefix}glossychrome
+${fx} ${prefix}greenbush
+${fx} ${prefix}metallogo
+${fx} ${prefix}noeltext
+${fx} ${prefix}glittergold
+${fx} ${prefix}textcake
+${fx} ${prefix}starsnight
+${fx} ${prefix}wooden3d
+${fx} ${prefix}textbyname
+${fx} ${prefix}writegalacy
+${fx} ${prefix}galaxybat
+${fx} ${prefix}snow3d
+${fx} ${prefix}birthdayday
+${fx} ${prefix}gplybutton
+${fx} ${prefix}splybutton
+${fx} ${prefix}epep
+${fx} ${prefix}blackpink
+${fx} ${prefix}neon
+${fx} ${prefix}greenneon
+${fx} ${prefix}advanceglow
+${fx} ${prefix}futureneon
+${fx} ${prefix}sandwriting
+${fx} ${prefix}sandsummer
+${fx} ${prefix}sandengraved
+${fx} ${prefix}metaldark
+${fx} ${prefix}neonlight
+${fx} ${prefix}holographic
+${fx} ${prefix}text1917
+${fx} ${prefix}minion
+${fx} ${prefix}deluxesilver
+${fx} ${prefix}newyearcard
+${fx} ${prefix}bloodfrosted
+${fx} ${prefix}halloween
+${fx} ${prefix}fireworksparkle
+${fx} ${prefix}natureleaves
+${fx} ${prefix}bokeh
+${fx} ${prefix}toxic
+${fx} ${prefix}strawberry
+${fx} ${prefix}box3d
+${fx} ${prefix}roadwarning
+${fx} ${prefix}breakwall
+${fx} ${prefix}icecold
+${fx} ${prefix}luxury
+${fx} ${prefix}cloud
+${fx} ${prefix}summersand
+${fx} ${prefix}horrorblood
+${fx} ${prefix}thunder2
+┬𖧹
+╰────────𖧹
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+╭──𖧹「 *INFO BOT* 」
+┴𖧹
+${fx} Author: FxSx/FdlX
+${fx} Recode: Pinky
+${fx} Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──𖧹「 *OWNER MENU* 」
+┴𖧹
+${fx} ${prefix}addprem
+${fx} ${prefix}removeprem
+${fx} ${prefix}addbadᴡord
+${fx} ${prefix}delbadᴡord
+${fx} ${prefix}addbucin
+${fx} ${prefix}colong
+${fx} ${prefix}setmemlimit
+${fx} ${prefix}resetlimit
+${fx} ${prefix}setlimit
+${fx} ${prefix}setprefix
+${fx} ${prefix}setfx
+${fx} ${prefix}setwms
+${fx} ${prefix}setzeks
+${fx} ${prefix}settech
+${fx} ${prefix}setlol
+${fx} ${prefix}setnamebot
+${fx} ${prefix}setppbot
+${fx} ${prefix}setffoto
+${fx} ${prefix}scmd
+${fx} ${prefix}delcmd
+${fx} ${prefix}bc
+${fx} ${prefix}bcgc
+${fx} ${prefix}bcs
+${fx} ${prefix}bcgcs
+${fx} ${prefix}bcaudio
+${fx} ${prefix}bcgcaudio
+${fx} ${prefix}bcvid
+${fx} ${prefix}bcgcvid
+${fx} ${prefix}ban
+${fx} ${prefix}unban
+${fx} ${prefix}block
+${fx} ${prefix}unblock
+${fx} ${prefix}clearall
+${fx} ${prefix}delete
+${fx} ${prefix}clone
+${fx} ${prefix}leave
+${fx} ${prefix}join
+┬𖧹
+╰────────𖧹
+`
+}
+exports.donasiBot = (prefix) => {
+    return `
+╭──𖧹「 *DONASI* 」
+┴𖧹
+❏ Pulsa : 083815956151
+❏ Dana : 083818221226
+❏ Kalo Udah Donasi Chat Aja
+❏ https://wa.me/c/16202033728
+❏ Nanti Ku Premium Kan 
+┬𖧹
+╰────────𖧹
+`
+}
